df444ab014658084b05615376be1c27b297ea04e
SJOSHI7 <sjoshise7@gmail.com>
2019-05-01 16:57:11 +0530

    refactored code

--- index.js
+++ index.js
@@ -14,24 +14,25 @@ fs.readdir(dirPath,function(err,files){
     if(err){
         return console.log('Unable to scan directory: ', err);
     }
-    
-    var fileExt=[];
-    var fileType=[];
+    //array for storing extensions 
+    var fileExt=[]; //contains duplicate elements too
+    var fileType=[]; //only contains unique elements
 
+    //copying extname in fileExt
     for(var i=0;i<files.length;i++){
         fileExt[i]=path.extname(files[i]);
     }
 
+    //removing empty string from array
     fileExt = fileExt.filter(Boolean)
 
+    //pushing unique values in fileType and creating folders for unique extensions    
     for(var value of fileExt){
         if(fileType.indexOf(value)===-1){
-            
             fileType.push(value);
-
             try {
-                if (!fs.existsSync(value)){
-                  fs.mkdirSync(value)
+                if (!fs.existsSync("All_"+value+"._Files")){
+                  fs.mkdirSync("All_"+value+"._Files");
                 }
               } catch (err) {
                 console.error(err)
